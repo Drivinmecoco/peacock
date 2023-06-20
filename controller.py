eaa80d899c897cd5b935d98739cd7095e98e9e41
maximweill <129801219+Drivinmecoco@users.noreply.github.com>
2023-06-20 14:12:18 +0100

    Update controller.py

--- controller.py
+++ controller.py
@@ -4,13 +4,14 @@ from neopixel import NeoPixel
 
 #formulas
 def volume2stage(volume):
-    step = 0.2
-    stage = min(volume/step,8)
+    step = 200
+    stage = min(int(volume/step),8)
     return stage
 
-def bass2angle(bass):
-    y = min(int(bass*90),90)
-    return [y,-y]
+def bass2angle(bass_v):
+    sensitivity = 1
+    y = min(int(bass_v*0.05*sensitivity),90)
+    return [y,90-y]
 
 
 #apply calculated angles to servos
@@ -23,15 +24,14 @@ def move_servos(angles):
 #applies calculated light stage to leds
 leds = NeoPixel(Pin(4),8)
 def light_leds(stage):
-    white = (255,255,255)
+    black = (0,0,0)
+    for i in range(8):
+        leds[i] = black
+
+    white = (40,40,40)
     for i in range(0,stage):
         leds[i] = white
     leds.write()
-
-#what to do each frame
-def frame(volume,bass):
-    #volume2LED(volume)
-    move_servos(bass2angle(bass))
 
 #microphone
 microphone = ADC(Pin(34))
@@ -52,6 +52,19 @@ def avg(lst,offset,l):
         [abs(lst[i]-offset) for i in range(len(lst)-1,len(lst)-l-1,-1)]
         )/l
 
+def dc(lst):
+    l=min(40,len(lst))
+    return sum(
+        [lst[i] for i in range(len(lst)-1,len(lst)-l-1,-1)]
+        )/l
+
+
+#what to do each frame
+def frame(volume,bass_v):
+    light_leds(volume2stage(volume))
+    move_servos(bass2angle(bass_v))
+
+
 def main():
     timer = 0
     FPS = 24
@@ -61,6 +74,7 @@ def main():
     b_l = 10
     basses = [2500 for i in range(b_l)]#0.1s
 
+    move_servos([0,90])
 
     run = True
     while run:
@@ -68,27 +82,33 @@ def main():
         volume = get_volume()
         volumes.append(volume)
 
-        dc_v = sum(volumes)/len(volumes)
+        dc_v = dc(volumes)
         avg_v = avg(volumes,dc_v,v_l)
 
 
-        bass_volume = get_bass()
+        bass_volume = get_volume()#get_bass()
         basses.append(bass_volume)
-        dc_b = sum(basses)/len(basses)
+        dc_b = dc(basses)
         avg_b = avg(basses,dc_b,b_l)
 
         #print(tuple([avg_v,abs(volume-dc_v)]))
         #print(tuple([avg_b,abs(bass_volume-dc_b)]))
-        print(tuple([avg_b,avg_v]))
+        #print(tuple([avg_b,avg_v,bass2angle(avg_b)[0]]))
 
         #if you hear a loud sound activate for at least 2s
-        if(avg_v>1):
+        if avg_v>0:
             timer = 2
 
         #as long as you heard a sound 2s ago, keep playing
         if timer>0:
-            pass
-            #frame(avg_v,avg_b)
+            frame(avg_v,avg_b)
+
+        if len(volumes)>100:
+            volumes.pop(0)
+        if len(basses)>100:
+            basses.pop(0)
+
+        print(tuple(bass2angle(avg_b)[1]))
 
         #frame rate
         sleep(1/FPS)
@@ -96,10 +116,10 @@ def main():
 
 main()
 
-"""
 def test():
-    while True:
-        #print(tuple([get_volume()]))
-        sleep(0.01)
+    print("done")
+    move_servos([0,90])
+    sleep(1)
+    move_servos([90,40])
 #test()
-"""
+
