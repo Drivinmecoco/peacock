1d6b9d5efb1e4bc9062fbbc302c15a3cfb1c4599
maximweill <129801219+Drivinmecoco@users.noreply.github.com>
2023-06-20 18:59:38 +0100

    Update controller.py

--- controller.py
+++ controller.py
@@ -4,27 +4,11 @@ from neopixel import NeoPixel
 
 #formulas
 def volume2stage(volume):
-    """evaluates the light stage that corresponds to a volume
-
-    Args:
-        volume (float): the volume measured in millivolts
-
-    Returns:
-        int: the stage
-    """
     step = 200
     stage = min(int(volume/step),8)
     return stage
 
 def bass2angle(bass_v):
-    """evaluates the angle that corresponds to a bass volume
-
-    Args:
-        bass_v (float): the volume measured in millivolts
-
-    Returns:
-        list: the first angle and the second servo angle
-    """
     sensitivity = 1
     angle = min(int(bass_v*0.05*sensitivity),90)
     return [angle,90-angle]
@@ -34,23 +18,12 @@ def bass2angle(bass_v):
 servo_0 = PWM(Pin(32), freq = 50)
 servo_1 = PWM(Pin(33), freq = 50)
 def move_servos(angles):
-    """moves the servos to the desired angle
-
-    Args:
-        angles (list): the two angle
-    """
     servo_0.duty(angles[0])
     servo_1.duty(angles[1])
 
 #applies calculated light stage to leds
 leds = NeoPixel(Pin(4),8)
 def light_leds(stage):
-    """lights up the leds to the right level
-    from bottom to top
-
-    Args:
-        stage (int): the number of leds to light up
-    """
     black = (0,0,0)
     for i in range(8):
         leds[i] = black
@@ -64,11 +37,6 @@ def light_leds(stage):
 microphone = ADC(Pin(34))
 microphone.atten(ADC.ATTN_11DB)
 def get_volume():
-    """fetches the microphone volume
-
-    Returns:
-        float: millivolts
-    """
     volt = microphone.read()
     return volt
 
@@ -76,38 +44,15 @@ def get_volume():
 bass = ADC(Pin(39))
 bass.atten(ADC.ATTN_11DB)
 def get_bass():
-    """fetches the filtered sound's volume
-
-    Returns:
-        float: millivolts
-    """
     volt = bass.read()
     return volt
 
 def avg(lst,offset,l):
-    """averages the last few elements of a list
-
-    Args:
-        lst (list): the input list
-        offset (float): the number to remove from each value
-        l (int): the number of element that should be averaged
-
-    Returns:
-        _type_: the average
-    """
     return sum(
         [abs(lst[i]-offset) for i in range(len(lst)-1,len(lst)-l-1,-1)]
         )/l
 
 def dc(lst):
-    """calculates the dc offset
-
-    Args:
-        lst (_type_): the voltages
-
-    Returns:
-        _type_: the average of the last 40
-    """
     l=min(40,len(lst))
     return sum(
         [lst[i] for i in range(len(lst)-1,len(lst)-l-1,-1)]
@@ -116,20 +61,11 @@ def dc(lst):
 
 #what to do each frame
 def frame(volume,bass_v):
-    """runs this function each frame
-
-    Args:
-        volume (float): total perceived volume
-        bass_v (float): smoother volume
-    """
     light_leds(volume2stage(volume))
     move_servos(bass2angle(bass_v))
 
 
 def main():
-    """
-        this is the main function
-    """
     timer = 0
     fps = 24
 
@@ -176,17 +112,10 @@ def main():
         sleep(1/fps)
         timer -= 1/fps
 
-#main()
+main()
 
 def test():
-    """
-    this is the function that is used
-    for debugging.
-    """
     print("done")
-    for i in range(5)
-        move_servos([10,10])
-        sleep(0.5)
-        move_servos([90,40])
+    move_servos([0,90])
     print("done")
-test()
+#test()
